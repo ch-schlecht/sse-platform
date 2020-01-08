26c9a5a149c65503f670309afde85c3fbb26d9f4
Paul Starke <ps83fitu@studserv.uni-leipzig.de>
2020-01-08 16:57:59 +0100

    update simple ajax

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,17 +1,25 @@
-var url = "localhost:8888"
-$(function (){
 
-    var $modules = $('#modules');
+var baseUrl = 'http://localhost:8888';
 
-    $.ajax({
+$(function () {
+
+      var $modules = $('#modules');
+
+      $.ajax({
         type: 'GET',
-        url: this.url + "/modules/list_available",
+        url: baseUrl + '/modules/list_available',
         dataType: 'json',
-        success: function(modules) {
-            $.each(modules, function(i, module){
-                $modules.append('<li> name: '+ module + '</li>');
-            });
-        }
-    });
+        success: function (modules) {
+          console.log(modules);
+          $.each(modules.modules, function (i, module) {
+            $modules.append('<li> ' + module + '</li>');
+          });
+        },
 
-});
+        error: function (xhr, status, error) {
+          console.log(error);
+          console.log(status);
+          console.log(xhr);
+        },
+      });
+    });
--- main.py
+++ main.py
@@ -199,7 +199,8 @@ def make_app():
         (r"/modules/([a-zA-Z\-0-9\.:,_]+)", ModuleHandler),
         (r"/configs/([a-zA-Z\-0-9\.:,_]+)", ConfigHandler),
         (r"/execution/([a-zA-Z\-0-9\.:,_]+)", ExecutionHandler),
-        (r"/css/(.*)", tornado.web.StaticFileHandler, {"path": "./css/"})
+        (r"/css/(.*)", tornado.web.StaticFileHandler, {"path": "./css/"}),
+        (r"/javascripts/(.*)", tornado.web.StaticFileHandler, {"path": "./javascripts/"})
     ])
 
 
